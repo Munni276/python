7cb703a5ef1afb0a44a9ac6ac4fa5a2464de50f4
Munni276 <devarapallimunni65@gmail.com>
2026-09-24 11:00:09 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,33 @@
+print("hello class")
+age=int(input("enter a age"))
+if(age>=18):
+    print("eligiable for vote")
+    print("eligiable for bike ride")
+else:
+    print("not eligiable for vote")
+    print("not eligiable for bike ride")
+    print("enter your name")
+
+
+
+
+
+
+    number=int(input("enter a number:"))
+    if(number==1):
+        print("MRD")
+    elif(number==2):
+        print("MBU")
+    elif(number==3):
+        print("Gitam")
+    else:
+        print("2")
+
+
+
+
+        number=int(input("enter a number:"))
+        if(number>0):  #45>0--t   100>0--t -9>0--f
+            if(number<50):  #45<50---t 100<50--f
+                print("number in between 1 - 50")
+                 
